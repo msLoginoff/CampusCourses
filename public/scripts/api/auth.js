da61460fc294b046381d94a9488af7cb555b508e
a.feofilov <a.feofilov@tinkoff.ru>
2024-12-29 03:57:58 +0700

    fix error handling

--- public/scripts/api/auth.js
+++ public/scripts/api/auth.js
@@ -7,7 +7,7 @@ export const login = async (email, password) => {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ email, password }),
+            body: JSON.stringify({email, password}),
         });
         if (!response.ok) {
             throw new Error('Ошибка авторизации');
@@ -22,30 +22,29 @@ export const login = async (email, password) => {
 };
 
 export const register = async (userData) => {
-    try {
-        const response = await fetch('https://camp-courses.api.kreosoft.space/registration', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(userData),
-        });
-        if (!response.ok) {
-            throw new Error('Ошибка регистрации');
-        }
-        const data = await response.json();
-        localStorage.setItem('authToken', data.token);
-        await renderNavbar();
-        return data;
-    } catch (error) {
-        console.error('Ошибка при регистрации:', error);
-        throw error;
+    const response = await fetch('https://camp-courses.api.kreosoft.space/registration', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+    });
+
+    if (!response.ok) {
+        console.error('Ошибка при регистрации: ' + JSON.stringify(await response.json()));
+        throw new Error(response.status.toString());
     }
+
+    const data = await response.json();
+    localStorage.setItem('authToken', data.token);
+    await renderNavbar();
+
+    return data;
 };
 
 export const fetchRoles = async () => {
     const token = localStorage.getItem('authToken');
-    if (!token) return { isTeacher: false, isStudent: false, isAdmin: false };
+    if (!token) return {isTeacher: false, isStudent: false, isAdmin: false};
 
     try {
         const response = await fetch('https://camp-courses.api.kreosoft.space/roles', {
@@ -60,7 +59,7 @@ export const fetchRoles = async () => {
         return await response.json();
     } catch (error) {
         console.error('Ошибка при получении ролей:', error);
-        return { isTeacher: false, isStudent: false, isAdmin: false };
+        return {isTeacher: false, isStudent: false, isAdmin: false};
     }
 };
 
